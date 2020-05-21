226a5fd4ce667bc2768c7d6e63bd615ed068a441
Sean Wu <seanwu1105@gmail.com>
2020-05-21 16:20:39 +0800

    Decide to set diagram mode from MainController.

--- src/main/java/io/github/seanwu1105/umldiagrameditor/diagram/Diagram.java
+++ src/main/java/io/github/seanwu1105/umldiagrameditor/diagram/Diagram.java
@@ -1,6 +1,11 @@
 package io.github.seanwu1105.umldiagrameditor.diagram;
 
+import io.github.seanwu1105.umldiagrameditor.diagram.mode.Mode;
 import javafx.scene.layout.Pane;
+import org.jetbrains.annotations.NotNull;
 
 public class Diagram extends Pane {
+
+    public void setMode(@NotNull final Mode mode) {
+    }
 }
