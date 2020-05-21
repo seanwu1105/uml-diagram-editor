8b6ac42deef1fd1254c1b077c1581bfe5d73ec73
Sean Wu <seanwu1105@gmail.com>
2020-05-22 00:20:52 +0800

    Inject Diagram into Canvas.

--- src/main/java/io/github/seanwu1105/umldiagrameditor/canvas/Canvas.java
+++ src/main/java/io/github/seanwu1105/umldiagrameditor/canvas/Canvas.java
@@ -2,17 +2,26 @@ package io.github.seanwu1105.umldiagrameditor.canvas;
 
 import io.github.seanwu1105.umldiagrameditor.canvas.mode.Mode;
 import io.github.seanwu1105.umldiagrameditor.canvas.mode.ModeFactory;
+import io.github.seanwu1105.umldiagrameditor.diagram.Diagram;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import org.jetbrains.annotations.NotNull;
 
-public class Canvas extends Pane {
+public final class Canvas extends Pane {
 
+    @NotNull
+    private Diagram diagram;
     @NotNull
     private Mode mode = ModeFactory.createSelectMode();
 
-    public Canvas() {
+    private Canvas(@NotNull final Diagram diagram) {
+        this.diagram = diagram;
         addEventFilter(MouseEvent.MOUSE_PRESSED, event -> mode.onMousePressed(event));
+    }
+
+    @NotNull
+    public static Canvas getInstance() {
+        return new Canvas(new Diagram());
     }
 
     public void setMode(@NotNull final Mode mode) {
